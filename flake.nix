053e2fa2cb64d519199757c621c39e32fd2ea583
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-15 20:40:09 -0500

    Getting interesting

--- flake.nix
+++ flake.nix
@@ -21,16 +21,20 @@
     rootfs = pkgs.stdenvNoCC.mkDerivation {
       name = "wnix-rootfs";
       dontUnpack = true;
-      nativeBuildInputs = [ busybox ];
+      dontFixup = true;
+      dontPatchShebangs = true;
+      nativeBuildInputs = [ ];
       installPhase = ''
         set -euo pipefail
         mkdir -p $out/{bin,etc/nix,etc/ssl/certs,tmp,nix/store}
         chmod 1777 $out/tmp
 
-        cp -v ${busybox}/bin/busybox $out/bin/busybox
-        cp -v ${bash}/bin/bash $out/bin/sh
         ln -s ${nix}/bin/nix $out/bin/nix
         ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt $out/etc/ssl/certs/ca-bundle.crt
+
+        cp -v ${busybox}/bin/busybox $out/bin/busybox
+        ln -sf busybox $out/bin/sh
+        $out/bin/busybox --install -s $out/bin
 
         cat > $out/etc/os-release <<'EOF'
         ID=wnix
@@ -55,6 +59,7 @@
         EOF
 
         cat > $out/init <<'EOF'
+        #!/bin/sh
         export PATH=/bin
         export HOME=/root
 
@@ -167,7 +172,7 @@
               -m 1024 -nographic \
               -kernel ${kernel}/bzImage \
               -initrd ${initramfs} \
-              -append "console=ttyS0 init=/init" \
+              -append "console=ttyS0 rdinit=/init" \
               -nic user,model=virtio-net-pci
           '';
         });
